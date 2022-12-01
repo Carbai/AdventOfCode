8374e5d5eb3c74ce319aed21251228fefbec8fa0
Carmen Baiano <61969673+Carbai@users.noreply.github.com>
2022-12-01 17:13:48 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 with open('day1.txt') as f1:
     lines = f1.readlines()
-    my_dict = {'elves': [],
-               'calories': []}
+my_dict = dict()
 
-    if lines[0] != "\n":
-        my_dict['elves'].append(1)
-        my_dict['calories'].append(int(lines[0]))
-        blank = False
-    else:
-        print('Sorry no elves made it! Christmas is ruined.')
+if lines[0] != "\n":
+    my_dict[0] = int(lines[0])
+    blank_line = False
+else:
+    print('Sorry no elves made it! Christmas is ruined.')
+    exit()
 
-    for i in range(1, len(lines)):
-        if lines[i] != "\n" and blank == False:
-            cal = my_dict.get('calories')
-            cal[-1] += int(lines[i])
-            my_dict['calories'] = cal
+for i in range(1, len(lines)):
+    if lines[i] != "\n" and blank_line == False:
+        my_dict[len(my_dict)-1] = my_dict[len(my_dict)-1] + int(lines[i])
 
-        elif lines[i] != "\n" and blank == True:
-            new_elf = len(my_dict['elves']) + 1
-            my_dict['elves'].append(new_elf)
-            my_dict['calories'].append(int(lines[i]))
-            blank = False
+    elif lines[i] != "\n" and blank_line == True:
 
-        elif lines[i] == "\n":
-            blank = True
-    snacks = my_dict.get('calories')
-    snacks.sort(reverse=True)
-    three_top = sum(snacks[0:3])
+        my_dict[len(my_dict)] = len(my_dict)
 
-    print('Highest snacks amount as one elf property:', max(my_dict.get('calories')))
-    print('Total snacks owned by the top three snacks holders:', three_top)
+        my_dict[len(my_dict)-1] = int(lines[i])
+        blank_line = False
+
+    elif lines[i] == "\n":
+        blank_line = True
+
+print(my_dict)
+sorted_snacks = dict(sorted(my_dict.items(), key=lambda item: item[1]))
+three_richest_elves = list(sorted_snacks.items())[-3:]
+tot = [sum(i) for i in zip(*three_richest_elves)]
+print("Highest amount of snacks as single elf property:", three_richest_elves[-1][-1])
+print("Highest amount of snacks as three richest elves property:", tot[-1])
+
+
