06c9a267d65d39b4e7a6df1b5ca5f038abd17f24
Carbai <carmen.baiano@sns.it>
2022-12-04 19:21:30 +0100

    Refactoring

--- Day_4/main.py
+++ Day_4/main.py
@@ -12,20 +12,24 @@ def parse_turns(turns: list) -> tuple:
     return (range_1, range_2)
 
 def part_one(turns: list) -> int:
-    overlap = 0
-    elf_1, elf_2 = parse_turns(turns)
-    for item1, item2 in zip(elf_1,elf_2):
-        if set(item1).issubset(item2) or set(item2).issubset(item1):
-            overlap +=1
-    return overlap
+    return compute_solution(turns, overlap_exists)
 
 def part_two(turns: list) -> int:
+    return compute_solution(turns, intersection_exists)
+
+def compute_solution(turns: list, func) -> int:
     overlap = 0
     elf_1, elf_2 = parse_turns(turns)
     for item1, item2 in zip(elf_1,elf_2):    
-        if len(set(item1).intersection(item2)) != 0:
+        if func(item1, item2):
             overlap += 1        
     return overlap
+
+def overlap_exists(item1, item2):
+    return set(item1).issubset(item2) or set(item2).issubset(item1)
+
+def intersection_exists(item1, item2):
+    return len(set(item1).intersection(item2)) != 0
 
 print(part_one(lines))
 print(part_two(lines))
